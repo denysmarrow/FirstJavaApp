2e2506c0d54fe0e11c3b8ef01af0a6af0042a22f
denysmarrow <mzmarrowz@gmail.com>
2021-12-20 18:56:55 -0500

    update

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,8 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getFirstElement(new int[] {3,1,4}));
+        System.out.println(getFirstElement(new int[] {2,1,4}));
+        System.out.println(getFirstElement(new int[] {5,1,4}));
+        System.out.println(getFirstElement(new int[] {7,1,4}));
     }
     public static int getFirstElement(int[] arr){
         return arr[0];
+    }
+
+    public static int addition(int num) {
+        return num +1;
+        System.out.println(addition);
+    }
+
+    public static int search(int arr[], int item) {
+        return arr.indexOf(item, 0)
     }
 }
